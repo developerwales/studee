09768eb7829e159664c56f9d20f3aa7fecc98ba3
Steven Parsons <steve@autosonshow.tv>
2020-05-22 02:25:45 +0100

    Created Routes and Form for the country search

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('searchforms.country');
 });
+
+Route::get('/country/search','SearchForms\CountrySearchController@search')->name('country.search');
+
+
